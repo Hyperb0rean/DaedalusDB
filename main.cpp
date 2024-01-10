56f8205fc3232eedc20bec292d61b51a7f75e628
hyperb0rean <jewellin02@gmail.com>
2024-01-10 19:08:39 +0300

    Migrated to C++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <iostream>
 
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
-
+    std::cout << "Hello world \n";
     return 0;
 }

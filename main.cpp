7b8b011176f44edd42c05ad9b732368610519a1e
hyperb0rean <jewellin02@gmail.com>
2024-01-11 12:54:29 +0300

    minor

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@
 
 int main() {
     io::File f("test.txt");
-    f.Write(std::string{"Hello, world!\n"}, 10);
-    std::cout << f.Read(0, 15);
+    struct Word {
+        char w[5];
+    };
+    f.Write(std::string{"Hello, world!\n"});
+    Word p = f.Read<Word>();
+    for (size_t i = 0; i < 5; ++i) {
+        std::cout << p.w[i];
+    }
+    f.Write<int>(150, 60);
+    int val = f.Read<int>(60);
+    std::cout << std::endl << val << ' ' << f.GetSize();
     return 0;
 }

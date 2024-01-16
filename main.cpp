587c4890538559bad7e0fd859113713f6a328bbe
hyperb0rean <jewellin02@gmail.com>
2024-01-16 23:11:32 +0300

    carried tests

--- main.cpp
+++ main.cpp
@@ -2,71 +2,8 @@
 
 #include "database.h"
 
-struct A {
-public:
-    virtual ~A() {
-    }
-    virtual void Foo() {
-        throw error::NotImplemented("lol");
-    }
-};
-
-struct B : public A {
-    int value;
-
-public:
-    ~B() = default;
-    void Foo() override {
-        std::cout << value << '\n';
-    }
-};
-
 int main() {
-    // auto file = std::make_unique<mem::File>("test.txt");
-    // B test;
-    // test.value = 1;
-    // test.Foo();
-    // // 1
-    // file->Write<B>(test, 0, 8);
-    // B test2 = file->Read<B>(0, 8);
-    // test2.Foo();
-    // // 1
 
     auto file = std::make_shared<mem::File>("database.data");
-    auto coords = types::StructType("coords");
-    coords.AddField(types::PrimitiveType<double>("lat"));
-    coords.AddField(types::PrimitiveType<double>("lon"));
-
-    auto city = std::make_shared<types::StructType>("city");
-    city->AddField(types::PrimitiveType<std::string>("name"));
-    city->AddField(coords);
-
-    auto node = types::Struct("person");
-    node.AddFieldValue(types::Primitive<std::string>("name", "Greg"));
-    node.AddFieldValue(types::Primitive<std::string>("surname", "Sosnovtsev"));
-    node.AddFieldValue(types::Primitive<int>("age", 19));
-
-    node.Write(file, 0);
-    file->Write("Cool", 4, 0, 4);
-    file->Write(20, 22);
-
-    std::cerr << node.ToString() << '\n';
-    // person: { name: "Greg", surname: "Sosnovtsev" }
-    node.Read(file, 0);
-    std::cerr << node.ToString();
-    // person: { name: "Cool", surname: "Sosnovtsev" }
-
-    auto range = mem::PageRange(0, 5);
-    for (size_t i = 0; i < 5; ++i) {
-        file->Write<mem::Page>({mem::PageType::kAllocatorMetadata, i, 0, 60 + mem::kPageSize * i},
-                               60 + mem::kPageSize * i);
-    }
-
-    auto alloc = mem::PageAllocator(file, 3, 60, 0, 0);
-
-    for (auto page : alloc.GetPageRange(0, 5)) {
-        std::cerr << page.index << ' ';
-    }
-
     return 0;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,16 +4,41 @@
 
 #include "database.h"
 
-TEST(TestGroupName, Subtest_1) {
-    ASSERT_TRUE(1 == 1);
+TEST(TypeTests, SimpleReadWrite) {
+    auto file = std::make_shared<mem::File>("test.data");
+
+    auto node = types::Struct("person");
+    node.AddFieldValue(types::Primitive<std::string>("name", "Greg"));
+    node.AddFieldValue(types::Primitive<std::string>("surname", "Sosnovtsev"));
+    node.AddFieldValue(types::Primitive<int>("age", 19));
+
+    node.Write(file, 0);
+    file->Write("Cool", 4, 0, 4);
+    file->Write(20, 22);
+
+    ASSERT_EQ("person: { name: \"Greg\", surname: \"Sosnovtsev\", age: 19 }", node.ToString());
+    node.Read(file, 0);
+    ASSERT_EQ("person: { name: \"Cool\", surname: \"Sosnovtsev\", age: 20 }", node.ToString());
 }
 
-TEST(TestGroupName, Subtest_2) {
-    ASSERT_FALSE('b' == 'b');
-    std::cout << "continue test after failure" << std::endl;
+TEST(PageRangeTests, SimpleIteration) {
+    auto file = std::make_shared<mem::File>("test.data");
+
+    auto range = mem::PageRange(0, 5);
+    for (size_t i = 0; i < 5; ++i) {
+        file->Write<mem::Page>({mem::PageType::kAllocatorMetadata, i, 0, 60 + mem::kPageSize * i},
+                               60 + mem::kPageSize * i);
+    }
+
+    auto alloc = mem::PageAllocator(file, 60, 0, 0, 0);
+
+    int index = 0;
+    for (auto& page : alloc.GetPageRange(0, 5)) {
+        ASSERT_EQ(page.index, index++);
+    }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();

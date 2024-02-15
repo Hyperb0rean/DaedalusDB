f381c39a492eb7eda6233e7c1bf4ea39ff3ff934
hyperb0rean <jewellin02@gmail.com>
2024-02-15 22:34:56 +0300

    example

--- main.cpp
+++ main.cpp
@@ -3,25 +3,25 @@
 #include "database.hpp"
 
 int main() {
-    auto file =
-        std::make_shared<mem::File>("database.data", std::make_shared<util::ConsoleLogger>());
-
-    auto person_class = ts::NewClass<ts::StructClass>(
-        "person", ts::NewClass<ts::StringClass>("name"), ts::NewClass<ts::StringClass>("surname"),
-        ts::NewClass<ts::PrimitiveClass<int>>("age"),
-        ts::NewClass<ts::PrimitiveClass<bool>>("male"));
 
     auto coordinates_class = ts::NewClass<ts::StructClass>(
         "coordinates", ts::NewClass<ts::PrimitiveClass<double>>("lat"),
         ts::NewClass<ts::PrimitiveClass<double>>("lon"));
 
-    auto city_class = ts::NewClass<ts::StructClass>("city", ts::NewClass<ts::StringClass>("name"),
-                                                    coordinates_class);
+    auto database = db::Database(std::make_shared<mem::File>("database.data"),
+                                 db::OpenMode::kDefault, CONSOLE_LOGGER);
+    database.AddClass(coordinates_class);
 
-    auto database =
-        db::Database(file, db::OpenMode::kDefault, std::make_shared<util::ConsoleLogger>());
-    database.AddClass(person_class);
-    // database.AddClass(coordinates_class);
-    // database.AddClass(city_class);
+    database.AddNode(ts::New<ts::Struct>(coordinates_class, 31., 55.));
+    database.AddNode(ts::DefaultNew<ts::Struct>(coordinates_class));
+
+    database.PrintAllClasses();
+    database.PrintAllNodes(coordinates_class);
+
+    database.RemoveNodesIf(coordinates_class, [](auto it) { return it.Id() != 0; });
+
+    database.PrintAllNodes(coordinates_class);
+    database.RemoveClass(coordinates_class);
+
     return 0;
 }
